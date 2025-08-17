8510b4a6246e9391029ce1d0dc58704406c825af
Beemil <valeskavr17@gmail.com>
2025-08-16 21:19:18 -0600

    Se colocaron las propiedades
    y el encendido y apagado

--- DispositivoElect/Program.cs
+++ DispositivoElect/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using DispositivoElect;
+
+Telefono telefono = new Telefono();
+telefono.Marca = "Samsung";
+telefono.Modelo = "Galaxy S23";
+telefono.EstaEncendido = true;
+
+Laptop laptop = new Laptop();
+laptop.Marca = "Dell";
+laptop.Modelo = "XPS 15";
+laptop.EstaEncendido = false;
+laptop.NivelBateria = 45;
+
+
+telefono.MostrarEstado();
+laptop.MostrarEstado();
+Console.WriteLine("Encendiendo el dispositivo...");
+Console.WriteLine();
+
+telefono.Encender();
+laptop.Encender();
+Console.WriteLine();
+
+telefono.MostrarEstado();
+laptop.MostrarEstado();
+Console.WriteLine("Apagando el dispositivo...");
+Console.WriteLine();
+
+telefono.Apagar();
+laptop.Apagar();
+Console.WriteLine();
+
+telefono.MostrarEstado();
+laptop.MostrarEstado();
+
+
+
+
