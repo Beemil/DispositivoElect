a554c8a2a8d8ffdc8dbac28cfd3eae806b807ae2
Beemil <valeskavr17@gmail.com>
2025-08-16 21:17:15 -0600

    interfaz

--- DispositivoElect/IDispositivoElectronico.cs
+++ DispositivoElect/IDispositivoElectronico.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace DispositivoElect
 {
-    internal interface IDispositivoElectronico
+    public interface IDispositivoElectronico
     {
+        void Encender();
+        void Apagar();
+        void MostrarEstado();
+
     }
 }
