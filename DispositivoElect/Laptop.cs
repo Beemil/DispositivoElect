7c0997bfc5c2daaf065491f71ae1fe2bc2864c14
Beemil <valeskavr17@gmail.com>
2025-08-16 21:18:10 -0600

    Creacion de la clase Laptop
    Usando if y else para controlar el encendido y apagado

---
+++ DispositivoElect/Laptop.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace DispositivoElect
+{
+    public class Laptop : IDispositivoElectronico
+    {
+        public String Marca { get; set; }
+        public String Modelo { get; set; }
+        public bool EstaEncendido { get; set; }
+        public int NivelBateria { get; set; }
+
+        public void Encender()
+        {
+            EstaEncendido = true;
+            Console.WriteLine($"La laptop {Marca} {Modelo} está encendida.");
+        }
+
+        public void Apagar()
+        {
+            EstaEncendido = false;
+            Console.WriteLine($"La laptop {Marca} {Modelo} está apagada.");
+        }
+
+        public void MostrarEstado()
+        {
+            string estado;
+            if (EstaEncendido)
+            {
+                estado = "encendida";
+            }
+            else
+            {
+                estado = "apagada";
+            }
+            Console.WriteLine($"La laptop {Marca} {Modelo} está {estado} y su nivel de batería es {NivelBateria}%.");
+        }
+    }
+}
